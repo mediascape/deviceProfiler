e2980c96a42f4b4e4b58fd043f8eb1c1b99d8f1b
itamayo <itamayo@vicomtech.org>
2017-03-03 17:21:21 +0100

    express test  added

---
+++ test/express.test.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+var profilingServer = require('deviceprofiler').listen(app);
+
